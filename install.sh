8f439f7b4407fc710622a9a8cfa58ff9083bc95d
Ryan Reece <ryan.reece@cern.ch>
2017-01-21 19:42:01 +0100

    ci

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "cp -r dotfiles/* ../"
-cp -r dotfiles/* ../
+echo "cp -r dotfiles/.* ../"
+cp -r dotfiles/.* ../
 
 echo "You may want to copy additional scripts from the plugins into the"
 echo "init directory to use them or copy your own there."
